15ba13d4253e3c4b9773b23162efcda7bfbcc280
Shunhankar Singh <whstatus71@gmail.com>
2025-12-19 23:28:50 +0530

    Add optimal solution of Power of two

--- basics/power_of_two.cpp
+++ basics/power_of_two.cpp
@@ -1,20 +1,20 @@
 // Problem: 231 - Power of Two
-// Leetcode: https://leetcode.com/problems/power-of-two/description/
+// Leetcode: https://leetcode.com/problems/power-of-two/
 
-// Approach: Keep dividing the number by 2 until it becomes 1 or cannot be divided
-// Time Complexity: O(log2(n)) - number of times n can be divided by 2
+// Solution Type: Bit Manipulation
+// Approach: A power of two(n) has exactly one set bit(1) in its binary representation.
+//           And every number which is one less than power of two(n-1) will have all bits as 1 
+//               and 1 bit less than that number which is power of 2(n).
+//           So, on taking 'AND' of n and (n-1) will result 0.
+// Time Complexity: O(1)
 // Space Complexity: O(1)
 
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0)
+        if (n <= 0)
             return false;
 
-        while(n % 2 == 0){
-            n /= 2;
-        }
-
-        return n == 1;
+        return (n & (n - 1)) == 0;
     }
 };
